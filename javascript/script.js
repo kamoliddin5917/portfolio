7788f8a4ba6fc735f98627657be132e010d62aff
toxir-brat <toxirli94@gmail.com>
2021-08-25 19:06:55 +0500

    media finished

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,8 @@
 const scrolBtnC = document.querySelector(".js-contact");
 const scrolBtnA = document.querySelector(".js-arrow");
+const hamburger = document.querySelector(".hamburder");
+const navbar = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar__list");
 
 window.addEventListener("scroll", () => {
   scrolBtnC.classList.toggle("btns-active", window.scrollY > 500);
@@ -9,4 +12,20 @@ window.addEventListener("scroll", () => {
 scrolBtnA.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+});
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("navbar-active");
+});
+
+navbar.addEventListener("click", (e) => {
+  if (e.target === navbar) {
+    navbar.classList.remove("navbar-active");
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("navbar-active");
+  });
 });
