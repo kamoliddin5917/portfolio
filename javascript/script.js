cbbfed2615a0f0e36af6b0a9d9e4870f5809438b
toxir-brat <toxirli94@gmail.com>
2021-08-29 18:18:42 +0500

    animation

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,7 @@ const scrolBtnA = document.querySelector(".js-arrow");
 const hamburger = document.querySelector(".hamburder");
 const navbar = document.querySelector(".navbar");
 const links = document.querySelectorAll(".navbar__list");
+const animations = document.querySelectorAll(".animation");
 
 window.addEventListener("scroll", () => {
   scrolBtnC.classList.toggle("btns-active", window.scrollY > 500);
@@ -28,4 +29,19 @@ links.forEach((link) => {
   link.addEventListener("click", () => {
     navbar.classList.remove("navbar-active");
   });
+});
+
+window.addEventListener("scroll", () => {
+  for (let i = 0; i < animations.length; i++) {
+    let windowHeight = window.innerHeight;
+    let animationTop = animations[i].getBoundingClientRect().top;
+    let animationPoint = 50;
+
+    if (animationTop < windowHeight - animationPoint) {
+      animations[i].classList.add("animation-active");
+    } else {
+      animations[i].classList.contains("animation-active") &&
+        animations[i].classList.remove("animation-active");
+    }
+  }
 });
